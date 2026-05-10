4406be994a81cb2a2eec7a64276d57f209f354ff
yusakutak <yusaku2004.524@gmail.com>
2026-05-11 01:57:24 +0900

    cuisinetypesのスペル修正

--- db/migrate/20260412061118_create_user_cuisine_preferences.rb
+++ db/migrate/20260412061118_create_user_cuisine_preferences.rb
@@ -1,6 +1,6 @@
-class CreateUserPreferences < ActiveRecord::Migration[8.0]
+class CreateUserCuisinePreferences < ActiveRecord::Migration[8.0]
   def change
-    create_table :user_preferences do |t|
+    create_table :user_cuisine_preferences do |t|
       t.integer  :user_id,             null: false
       t.integer  :cuisine_type_id,     null: false
       t.integer  :score,               null: false, default: 0

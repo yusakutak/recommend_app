ce7a559750e080671cde1f26a80bc65e66d33adf
yusakutak <yusaku2004.524@gmail.com>
2026-05-11 15:00:02 +0900

    routes.rb修正完了

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,8 @@
 class RestaurantsController < ApplicationController
   before_action :authenticate_user!
-
+  def index
+    @restaurants = []  # 後でホットペッパーAPI繋ぐ
+  end
   def show
     @restaurant = Restaurant.find(params[:id])
     @reviews = @restaurant.reviews.includes(:user).order(created_at: :desc)
--- config/routes.rb
+++ config/routes.rb
@@ -5,26 +5,36 @@ Rails.application.routes.draw do
     confirmations: "users/confirmations"
   }
 
-  resources :users, only: [:show]
-  resources :friendships, only: [:create, :destroy] do
+  root "pages#mypage"
+
+  # ユーザー
+  resources :users, only: [ :show ]
+
+  # フレンド
+  resources :friendships, only: [ :index, :create, :destroy ] do
     member do
       patch :accept
     end
   end
-  resources :groups do
-    resources :messages, only: [:create]
-    resources :group_members, only: [:create, :destroy]
-  end
-  resources :restaurants, only: [:show] do
-    resources :reviews, only: [:new, :create]
-    resources :visit_histories, only: [:create]
-  end
-  resources :feedbacks, only: [:new, :create]
 
-  get "/form", to: "survey#form", as: :form
-  get "/mypage", to: "survey#mypage", as: :mypage
-  get "/search", to: "survey#search", as: :search
-  post "/survey", to: "survey#update_preferences", as: :update_preferences
+  # グループ
+  resources :groups, only: [ :index, :show, :new, :create ] do
+    resources :messages,      only: [ :create ]
+    resources :group_members, only: [ :index, :create, :destroy ]
+  end
 
-  root "survey#home"
+  # 飲食店・レビュー
+  resources :restaurants, only: [ :index, :show ] do
+    resources :reviews,         only: [ :new, :create, :index ]
+  end
+
+  # 訪問履歴
+  resources :visit_histories, only: [ :index, :show ]
+
+  # 嗜好アンケート
+  get  "/form",   to: "user_cuisine_preferences#edit",   as: :form
+  post "/survey", to: "user_cuisine_preferences#update", as: :survey
+
+  # マイページ
+  get "/mypage", to: "pages#mypage", as: :mypage
 end
